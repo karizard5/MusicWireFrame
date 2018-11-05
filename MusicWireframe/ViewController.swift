db85ffcd4eb395b7a0f0ddec1fa103e70abc37eb
Reid, Jon Carl <jreid2@hk220-mac3.local>
2018-11-05 11:06:47 -0800

    Initial Commit

---
+++ MusicWireframe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicWireframe
+//
+//  Created by Reid, Jon Carl on 11/5/18.
+//  Copyright © 2018 Reid, Jon Carl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
